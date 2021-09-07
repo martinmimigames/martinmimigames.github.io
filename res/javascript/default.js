e3302a7896e1ee545e5ac7b62e712b9bae0546ad
martinmimi <82596011+wmartinmimi@users.noreply.github.com>
2021-09-07 14:49:43 +0100

    light mode and dark mode support

--- res/javascript/default.js
+++ res/javascript/default.js
@@ -1,4 +1,21 @@
 var _isMobile = /Mobi/i.test(window.navigator.userAgent);
+// Check to see if Media-Queries are supported
+var _mediaQuerySupported = window.matchMedia;
+
+function darkModeCheck(){
+
+    if (_mediaQuerySupported) {
+      // Check if the dark-mode Media-Query matches
+      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        return true;
+      }
+
+    }
+    return false;
+
+}
+
+
 function isMobile(){
        return _isMobile;
        }
