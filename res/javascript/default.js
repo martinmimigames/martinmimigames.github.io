29f08b679a78575e6d16e0f7b3a38c5f55138c64
martinmimi <82596011+wmartinmimi@users.noreply.github.com>
2021-09-07 15:28:44 +0100

    light mode and dark mode support test fix #2

--- res/javascript/default.js
+++ res/javascript/default.js
@@ -1,25 +1,4 @@
 var _isMobile = /Mobi/i.test(window.navigator.userAgent);
-// Check to see if Media-Queries are supported
-var _mediaQuerySupported;
-if (window.matchMedia){
-    _mediaQuerySupported = true;
-}else {
-    _mediaQuerySupported = false;
-}
-
-function darkModeCheck(){
-
-    if (_mediaQuerySupported) {
-      // Check if the dark-mode Media-Query matches
-      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-        return true;
-      }
-
-    }
-    return false;
-
-}
-
 
 function isMobile(){
        return _isMobile;
